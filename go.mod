0d6f872af2ace220058631cf5ca1fd914cfc4259
kpango <i.can.feel.gravity@gmail.com>
2019-12-15 02:27:58 +0900

    fix
    Signed-off-by: kpango <i.can.feel.gravity@gmail.com>

--- go.mod
+++ go.mod
@@ -4,5 +4,6 @@ go 1.13
 
 require (
 	github.com/gogo/protobuf v1.3.1
+	github.com/kpango/glg v1.4.6
 	google.golang.org/grpc v1.25.1
 )
--- main.go
+++ main.go
@@ -2,44 +2,51 @@ package main
 
 import (
 	"bytes"
+	"context"
+	"encoding/base64"
 	"encoding/json"
 	"image"
 	"image/draw"
+	"image/jpeg"
 	"image/png"
 	"io"
+	"io/ioutil"
 	"log"
-	"mime/multipart"
 	"net"
 	"net/http"
+	"sync"
 	"time"
 
+	"github.com/kpango/glg"
+	"github.com/teamnameis/be/bone"
 	"google.golang.org/grpc"
 )
 
-const (
-	port = "0.0.0.0:1234"
-	ml   = "https://127.0.0.1:5000/"
+var (
+	clothesMap sync.Map
+
+	httick = time.NewTicker(time.Millisecond * 200)
 )
+
+const (
+	grpcport = "0.0.0.0:5678"
+	port     = "0.0.0.0:1234"
+	ml       = "127.0.0.1:5000"
+)
+
+type Request struct {
+	ID   int32  `json:"id"`
+	Data string `json:"data"`
+}
 
 type MorphResponse struct {
 	Data []byte `json:"data"`
 }
 
 type MorphRequest struct {
-	UBones  *Bones `json:"user_bones"`
-	CBones  *Bones `json:"clothes_bones"`
+	User    []byte `json:"user"`
 	Clothes []byte `json:"clothes"`
 }
-
-type Edge struct {
-	Score float64 `json:"score"`
-	X     float64 `json:"x"`
-	Y     float64 `json:"y"`
-}
-
-type Bone []Edge
-
-type Bones []Bone
 
 // server is used to implement helloworld.GreeterServer.
 type server struct{}
@@ -48,12 +55,13 @@ type server struct{}
 func (s *server) Send(stream Overlay_SendServer) error {
 	first := true
 	tick := time.NewTicker(time.Millisecond * 200)
+	var clothes []byte
 	for {
 		select {
 		case <-stream.Context().Done():
 			return stream.Context().Err()
 		default:
-			flame, err := stream.Recv()
+			frame, err := stream.Recv()
 			if err != nil {
 				if err == io.EOF {
 					return nil
@@ -61,19 +69,30 @@ func (s *server) Send(stream Overlay_SendServer) error {
 				return err
 			}
 
-			if flame != nil {
-				select {
-				case <-tick.C:
-				default:
-					if first {
-						first = false
-						bones, err = bone(flame.Data)
+			if frame != nil {
+				err = func() error {
+					select {
+					case <-tick.C:
+						clothes, err = morph(frame.GetId(), frame.GetData())
 						if err != nil {
-							return err
+							return stream.Send(frame)
+						}
+					default:
+						if first {
+							first = false
+							clothes, err = morph(frame.GetId(), frame.GetData())
+							if err != nil {
+								return stream.Send(frame)
+							}
 						}
 					}
-				}
-				err = stream.Send(flame)
+					res, err := overlay(frame.Data, clothes)
+					if err != nil {
+						return stream.Send(frame)
+					}
+					frame.Data = res
+					return stream.Send(frame)
+				}()
 				if err != nil {
 					return err
 				}
@@ -83,15 +102,15 @@ func (s *server) Send(stream Overlay_SendServer) error {
 	return nil
 }
 
-func overlay(img1, img2 []byte) ([]byte, error) {
-	first, err := png.Decode(bytes.NewBuffer(img1))
+func overlay(person, clothes []byte) ([]byte, error) {
+	first, err := jpeg.Decode(bytes.NewBuffer(person))
 	if err != nil {
-		log.Fatalf("failed to decode: %s", err)
+		return nil, err
 	}
 
-	second, err := png.Decode(bytes.NewBuffer(img2))
+	second, err := png.Decode(bytes.NewBuffer(clothes))
 	if err != nil {
-		log.Fatalf("failed to decode: %s", err)
+		return nil, err
 	}
 
 	offset := image.Pt(0, 0)
@@ -107,69 +126,84 @@ func overlay(img1, img2 []byte) ([]byte, error) {
 	return buf.Bytes(), nil
 }
 
-func morph(id int, bones *Bones) ([]byte, error) {
-	var body *bytes.Buffer
-	err := json.NewEncoder(body).Encode(MorphRequest{
-		ID:    id,
-		Bones: bones,
+func morph(id int32, user []byte) ([]byte, error) {
+	conn, err := grpc.Dial(ml, grpc.WithInsecure())
+	if err != nil {
+		return nil, err
+	}
+	defer conn.Close()
+	data, err := bone.NewMLClient(conn).Morph(context.Background(), &bone.Frame{
+		Id:   id,
+		Data: user,
 	})
 	if err != nil {
 		return nil, err
 	}
-	res, err := http.Post(ml, "application/json", body)
-	if err != nil {
-		return nil, err
-	}
-	var data MorphResponse
-	err = json.NewDecoder(res.Body).Decode(&data)
-	if err != nil {
-		return nil, err
-	}
-	return data.Data, nil
-}
 
-func bone(b []byte) (bones *Bones, err error) {
-	body := &bytes.Buffer{}
-	mw := multipart.NewWriter(body)
-	w, err := mw.CreateFormField("image")
-	if err != nil {
-		return nil, err
-	}
-	_, err = w.Write(b)
-	if err != nil {
-		return nil, err
-	}
-	ct := mw.FormDataContentType()
-	err = mw.Close()
-	if err != nil {
-		return nil, err
-	}
-	res, err := http.Post(ml, ct, body)
-	if err != nil {
-		return nil, err
-	}
-	bones = new(Bones)
-	err = json.NewDecoder(res.Body).Decode(&bones)
-	if err != nil {
-		return nil, err
-	}
-	err = res.Body.Close()
-	if err != nil {
-		return nil, err
-	}
-	return bones, nil
+	return data.GetData(), nil
 }
 
 func main() {
-	lis, err := net.Listen("tcp", port)
+
+	var (
+		clothes []byte
+		first   = true
+	)
+
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		data := new(Request)
+		err := json.NewDecoder(r.Body).Decode(&data)
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusBadRequest)
+			return
+		}
+		io.Copy(ioutil.Discard, r.Body)
+		r.Body.Close()
+
+		if data != nil {
+			glg.Info(data.ID)
+			img, err := base64.StdEncoding.DecodeString(data.Data)
+			if err != nil {
+				json.NewEncoder(w).Encode(data)
+				return
+			}
+			select {
+			case <-httick.C:
+				clothes, err = morph(data.ID, img)
+				if err != nil {
+					json.NewEncoder(w).Encode(data)
+					return
+				}
+			default:
+				if first {
+					first = false
+					clothes, err = morph(data.ID, img)
+					if err != nil {
+						json.NewEncoder(w).Encode(data)
+						return
+					}
+				}
+			}
+			res, err := overlay(img, clothes)
+			if err != nil {
+				json.NewEncoder(w).Encode(data)
+				return
+			}
+			json.NewEncoder(w).Encode(Request{
+				Data: base64.StdEncoding.EncodeToString(res),
+			})
+		}
+	})
+	lis, err := net.Listen("tcp", grpcport)
 	if err != nil {
 		log.Fatalf("failed to listen: %v", err)
 	}
 	s := grpc.NewServer()
 	RegisterOverlayServer(s, &server{})
-	// Register reflection service on gRPC server.
-	// reflection.Register(s)
-	if err := s.Serve(lis); err != nil {
-		log.Fatalf("failed to serve: %v", err)
-	}
+	go func() {
+		if err := s.Serve(lis); err != nil {
+			log.Fatalf("failed to serve: %v", err)
+		}
+	}()
+	http.ListenAndServe(port, nil)
 }
